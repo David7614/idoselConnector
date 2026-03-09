0e2d980b2969541a7d4aecb2ba19ba7e03b137de
ktomek <tiacocstoe@gmail.com>
2026-03-09 18:55:53 +0100

    config for heroku

--- config/db.php
+++ config/db.php
@@ -1,14 +1,26 @@
 <?php
 
+if ($url = getenv('STACKHERO_MYSQL_DATABASE_URL')) {
+    $parsed = parse_url($url);
+    $host   = $parsed['host'];
+    $port   = $parsed['port'] ?? 3306;
+    $user   = $parsed['user'] ?? 'root';
+    $pass   = isset($parsed['pass']) ? urldecode($parsed['pass']) : '';
+    $dbName = getenv('DB_NAME') ?: 'samba_idosell';
+
+    return [
+        'class'    => 'yii\db\Connection',
+        'dsn'      => "mysql:host={$host};port={$port};dbname={$dbName}",
+        'username' => $user,
+        'password' => $pass,
+        'charset'  => 'utf8mb4',
+    ];
+}
+
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=samba_idosell',
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=samba_idosell',
     'username' => 'root',
     'password' => 'ABCabc123',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'charset'  => 'utf8',
 ];
