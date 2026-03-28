15099e4de2ee58af0cbc9655d3f6ce9a8bdbfcfb
ktomek <tiacocstoe@gmail.com>
2026-03-28 14:29:07 +0100

    migr2

--- migrations/m260328_160000_utf8mb4_full_database.php
+++ migrations/m260328_160000_utf8mb4_full_database.php
@@ -9,12 +9,17 @@ class m260328_160000_utf8mb4_full_database extends Migration
         $db = \Yii::$app->db;
         $dbName = $db->createCommand('SELECT DATABASE()')->queryScalar();
 
+        $originalMode = $db->createCommand('SELECT @@sql_mode')->queryScalar();
+        $this->execute("SET sql_mode = ''");
+
         $this->execute("ALTER DATABASE `{$dbName}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
 
         $tables = $db->createCommand("SHOW TABLES")->queryColumn();
         foreach ($tables as $table) {
             $this->execute("ALTER TABLE `{$table}` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
         }
+
+        $this->execute("SET sql_mode = '{$originalMode}'");
     }
 
     public function safeDown()
