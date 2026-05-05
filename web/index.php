7aa8ea6682075f81d9f186659a8e8a7a89a05fc8
ktomek <tiacocstoe@gmail.com>
2026-05-05 11:24:34 +0200

    https forwarder proto

--- web/index.php
+++ web/index.php
@@ -2,6 +2,10 @@
 
 require __DIR__ . '/../config/env.php';
 
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
